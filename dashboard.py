4b72ce08d431d04710502f5fc082210ae55c4c4d
taziobaker <taziobaker@gmail.com>
2021-11-19 08:52:06 -0500

    Upd

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 import config
 
+from helpers import format_number
+
 symbol = st.sidebar.text_input("Symbol", value="AMC")
 
 screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'News', 'Ownership', 'Technicals'))
@@ -9,14 +11,25 @@ screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'News', 'Owne
 st.title(screen)
 
 if screen == 'Overview':
-  url = f"https://cloud.iexapis.com/stable/stock/amc/logo?token={config.IEX_API_TOKEN}"
-  r = requests.get(url)
+  FREE = f"https://cloud.iexapis.com/stable/tops?token={config.IEX_API_TOKEN}&symbols={symbol}"
+  r = requests.get(FREE)
   response_json = r.json()
-  st.image(response_json['url'])
+  st.write
+  QUOTE = f"https://cloud.iexapis.com/stable/stock/{symbol}/quote?token={config.IEX_API_TOKEN}"
+  r = requests.get(QUOTE)
+  response_json = r.json()
+  
+  col1, col2 = st.beta_columns (2)
+  
+  with col1:
+    st.subheader.write("FREE:")
+    st.write(FREE)
+  
+  with col2:
+    st.subheader.write("QUOTE:")
+    st.write(QUOTE)
   
   
-    
-    #/stock/{symbol}/logo
 
 if screen == 'Fundamentals':
   pass
