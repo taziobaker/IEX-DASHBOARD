c1fa7a52f68fc5043312850d35f86e6c9f8db08d
taziobaker <taziobaker@gmail.com>
2021-11-19 10:39:20 -0500

    Upd

--- chart.py
+++ chart.py
@@ -7,7 +7,8 @@ import numpy as np
 #import yfinance as yf
 
 def load_data():
-  url = f"https://cloud.iexapis.com/stable/stock/amc/chart/1d?token={config.sk_05f62d0b97b84cfbb15650bc980058b0}"
+  #url = f"https://cloud.iexapis.com/stable/stock/amc/chart/1d?token={config.sk_05f62d0b97b84cfbb15650bc980058b0}"
+  url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
   hrml = pd.read_html(url, header = 0)
   df = html[0]
   return df
