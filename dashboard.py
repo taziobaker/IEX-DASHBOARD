29efa5c70ddf4b5b28b13e228e6a2e2797fc9fbc
taziobaker <taziobaker@gmail.com>
2021-11-19 08:04:47 -0500

    Create 1

---
+++ dashboard.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+import config
+
+symbol = st.sidebard.text_input("Symbol", value="AMC")
+
+screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'News', 'Ownership', 'Technicals'))
+
+st.title(screen)
+
+if screen == 'Overview':
+  url = f"https://cloud.iexapis.com/stable/stock/amc/logo?token={config.IEX_API_TOKEN}"
+  r = requests.get(url)
+  response_json = r.json()
+  st.image(response_json['url'])
+  
+  
+    
+    #/stock/{symbol}/logo
+
+if screen == 'Fundamentals':
+  pass
+
+# Free IEX price for AMC
+#https://cloud.iexapis.com/stable/tops?token=YOUR_TOKEN_HERE&symbols=amc
+#Stock quote for AMC
+#https://cloud.iexapis.com/stable/stock/amc/quote?token=YOUR_TOKEN_HERE
