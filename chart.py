58ca4f6e10d542a23f184ddb50de0501c1d40268
taziobaker <taziobaker@gmail.com>
2021-11-19 10:33:36 -0500

    Upd

--- chart.py
+++ chart.py
@@ -4,7 +4,7 @@ import base64
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 import numpy as np
-import yfinance as yf
+#import yfinance as yf
 
 def load_data():
   url = f"https://cloud.iexapis.com/stable/stock/amc/chart/1d?token={config.sk_05f62d0b97b84cfbb15650bc980058b0}"
